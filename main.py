d2cc98610fff375967c9f7d99d97622e948653ba
Mikkel Ziemer <mzhjensen@gmail.com>
2018-02-11 18:57:45 +0100

    Fixed some arg parsing

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ def main():
     tw = TwitterAnalysis()
     p = optparse.OptionParser(description='Executes a function on the Twitter data set',
                               usage='main.py -f [1-5]')
-    p.add_option("--function", "-f", action="store_true", help="Mandatory. Choose the function number to execute. It goes from 1 to 5, both inclusive")
-    p.add_option("--database", "-b", action="store_true", help="Optional. Set database address. Default is 'localhost'")
-    p.add_option("--port", "-p", action="store_true", help="Optional. Port of the database server/instance. Default is '27017'")
+    p.add_option("--function", "-f", help="Mandatory. Choose the function number to execute. It goes from 1 to 5, both inclusive")
+    p.add_option("--database", "-b", help="Optional. Set database address. Default is 'localhost'")
+    p.add_option("--port", "-p", help="Optional. Port of the database server/instance. Default is '27017'")
 
     options, arguments = p.parse_args()
 
@@ -27,10 +27,13 @@ def main():
             pprint.pprint(tw.number_of_users())
             return
         if options.function == '2':
+            print("Users mentioning others most")
             pprint.pprint(list(tw.users_mentioning_others_most()))
         if options.function == '3':
+            print("Most mentioned users")
             pprint.pprint(list(tw.most_mentioned_users()))
         if options.function == '4':
+            print("Top ten active users")
             pprint.pprint(list(tw.top_ten_active_users()))
         if options.function == '5':
             print("Most grumpy users are...")
